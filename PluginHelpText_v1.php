4e270005d51e73d32a4a74690a837b050375fb9f
Claes Östlund <claes@stenbergit.se>
2020-06-03 13:22:22 +0200

    1.3.0

--- PluginHelpText_v1.php
+++ PluginHelpText_v1.php
@@ -2,6 +2,7 @@
 class PluginHelpText_v1{
   public $settings;
   public $mysql;
+  private $i18n;
   function __construct($buto) {
     if($buto){
       wfPlugin::enable('wf/form_v2');
@@ -9,6 +10,12 @@ class PluginHelpText_v1{
       wfPlugin::includeonce('wf/mysql');
       $this->mysql =new PluginWfMysql();
       $this->settings = wfPlugin::getPluginSettings('help/text_v1', true);
+      /**
+       * 
+       */
+      wfPlugin::includeonce('i18n/translate_v1');
+      $this->i18n = new PluginI18nTranslate_v1();
+      $this->i18n->path = '/plugin/help/text_v1/i18n';
     }
   }
   public function getElement($name){
@@ -21,11 +28,12 @@ class PluginHelpText_v1{
     return new PluginWfYml(__DIR__.'/mysql/sql.yml', $key);
   }
   public function form_render($form){
+    $form = new PluginWfArray($form);
     $rs = $this->db_text_select_one(wfRequest::get('id'));
     $form->set('items/id/default', wfRequest::get('id'));
     $form->set('items/headline/default', $rs->get('headline'));
     $form->set('items/description/default', $rs->get('description'));
-    return $form;
+    return $form->get();
   }
   public function form_capture(){
     $rs = $this->db_text_select_one(wfRequest::get('id'));
@@ -36,7 +44,7 @@ class PluginHelpText_v1{
     if(wfRequest::get('clear')){
       $this->db_confirm_delete_by_text(wfRequest::get('id'));
     }
-    return array("$('#modal_helptext').modal('hide');alert('Item was saved. Please update page where helptext occured.');");
+    return array("$('#modal_helptext').modal('hide');alert('".$this->i18n->translateFromTheme('helptext_i18n_item_was_saved')."');");
   }
   public function db_open(){
     $this->mysql->open($this->settings->get('data/mysql'));
@@ -124,7 +132,7 @@ class PluginHelpText_v1{
     /**
      * Set session.
      */
-    $this->session_set($helptext_text_id);
+    $this->session_set(array('helptext_text_id' => $helptext_text_id, 'created_at' => date('Y-m-d H:i:s')));
     return null;
   }
   private function db_confirm_select_list(){
@@ -153,12 +161,16 @@ class PluginHelpText_v1{
     $this->mysql->execute($sql->get());
     return null;
   }
-  private function session_set($helptext_text_id){
-    $_SESSION['plugin']['help']['text_v1']['confirm'][$helptext_text_id] = true;
+  private function session_set($value){
+    $_SESSION['plugin']['help']['text_v1']['confirm'][$value['helptext_text_id']] = $value['created_at'];
     return null;
   }
   private function session_exist($helptext_text_id){
-    return isset($_SESSION['plugin']['help']['text_v1']['confirm'][$helptext_text_id]);
+    if(isset($_SESSION['plugin']['help']['text_v1']['confirm'][$helptext_text_id])){
+      return $_SESSION['plugin']['help']['text_v1']['confirm'][$helptext_text_id];
+    }else{
+      return null;
+    }
   }
   private function session_clear(){
     unset($_SESSION['plugin']['help']['text_v1']['confirm']);
@@ -198,8 +210,10 @@ class PluginHelpText_v1{
      */
     if($this->session_exist($rs->get('id'))){
       $rs->set('confirm', true);
+      $rs->set('created_at', $this->session_exist($rs->get('id')));
     }else{
       $rs->set('confirm', false);
+      $rs->set('created_at', null);
     }
     /**
      * Element.
@@ -239,14 +253,12 @@ class PluginHelpText_v1{
        * 
        */
       $text = null;
-      if($item->get('is_confirm')=='Yes' && $item->get('in_session')=='Yes'){
-        $text = 'Yes';
-      }elseif($item->get('is_confirm')=='Yes'){
-        $text = 'Only in database';
+      if($item->get('is_confirm')=='Yes'){
+        $text = $this->i18n->translateFromTheme('Permanent');
       }elseif($item->get('in_session')=='Yes'){
-        $text = 'Only in session';
+        $text = $this->i18n->translateFromTheme('Temporary');
       }else{
-        $text = 'No';
+        $text = $this->i18n->translateFromTheme('No');
         continue;
       }
       $box->setById('confirmed', 'innerHTML', $text);
@@ -261,6 +273,7 @@ class PluginHelpText_v1{
     /**
      * Render.
      */
+    $clear->setByTag(array('sizeof' => sizeof($element)));
     wfDocument::renderElement($clear->get());
     wfDocument::renderElement($element);
     
@@ -269,14 +282,17 @@ class PluginHelpText_v1{
     if(!wfUser::hasRole('webmaster')){
       exit('');
     }
-    $form = $this->getForm('text');
-    $widget = wfDocument::createWidget('wf/form_v2', 'render', $form->get());
-    wfDocument::renderElement(array($widget));
-    
+    /**
+     * 
+     */
     $rs = $this->db_confirm_select_by_text_count(wfRequest::get('id'));
-    $element = array();
-    $element[] = wfDocument::createHtmlElement('p', array(wfDocument::createHtmlElement('span', 'User confirms:'), wfDocument::createHtmlElement('span', $rs->get('count'))));
-    wfDocument::renderElement($element);
+    /**
+     * 
+     */
+    $form = $this->getForm('text');
+    $form->setByTag($rs->get());
+    $widget = wfDocument::createWidget('form/form_v1', 'render', $form->get());
+    wfDocument::renderElement(array($widget));
   }
   public function page_capture(){
     if(!wfUser::hasRole('webmaster')){
@@ -289,7 +305,7 @@ class PluginHelpText_v1{
   public function event_signin(){
     $rs = $this->db_confirm_select_list();
     foreach ($rs as $key => $value) {
-      $this->session_set($value['helptext_text_id']);
+      $this->session_set($value);
     }
     return null;
   }
